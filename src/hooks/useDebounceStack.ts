fa5ab138bd008d7c2ce31613e2a003184f023232
Paul Thomas <paul@asup.co.uk>
2024-06-06 10:04:32 +0100

    npm update, version bump @ release 1.3.0

--- src/hooks/useDebounceStack.ts
+++ src/hooks/useDebounceStack.ts
@@ -145,7 +145,12 @@ export const useDebounceStack = <T>(
 
   // Update value from debouncedValue, if the value has not changed since the last update
   useEffect(() => {
-    if (debouncedValue !== null && !isEqual(debouncedValue, value) && isEqual(value, lastValue)) {
+    if (
+      debouncedValue !== null &&
+      debouncedValue !== undefined &&
+      !isEqual(debouncedValue, value) &&
+      isEqual(value, lastValue)
+    ) {
       setCurrentValue(debouncedValue);
       setValue(debouncedValue);
       onDebounce(debouncedValue);
